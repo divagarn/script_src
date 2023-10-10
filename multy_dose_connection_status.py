b98ef15e152beb68412259d9524a45529b00afde
divagarn <n.divagar@mobiveil.co.in>
2023-10-10 12:34:28 +0530

    only the connection status of multiple devices

---
+++ multy_dose_connection_status.py
@@ -0,0 +1,92 @@
+import pygatt
+import struct
+import rospy
+import time
+import threading
+import subprocess
+
+rospy.init_node("data_publisher")
+device_macs = ["AD:C3:5A:FA:42:79", "83:0D:B2:C9:53:9B", "21:A3:F6:9D:E6:CC"]
+characteristic_uuid = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+max_retries = 10
+
+adapters = [pygatt.GATTToolBackend() for _ in range(len(device_macs))]
+
+for adapter in adapters:
+    adapter.start()
+    time.sleep(2)
+
+device_obj_list = []
+dosage_data = []
+
+# Initialize flags for each device
+device_flags = [False] * len(device_macs)
+
+
+def check_bluetooth_device_status(device_mac):
+    try:
+        output = subprocess.check_output(["bluetoothctl", "info", device_mac], stderr=subprocess.STDOUT, text=True)
+
+        if "Connected: yes" in output:
+            return "Connected"
+        elif "Paired: yes" in output:
+            return "Paired but not connected"
+        else:
+            return "Disconnected"
+
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e}"
+
+
+def connect_with_retries(device_mac, adapter, index):
+    mr = 0
+    while not rospy.is_shutdown() and mr < max_retries:
+        try:
+            device = adapter.connect(device_mac)
+            device_obj_list.append(device)
+            print(f"{device_mac} CONNECTED")
+            # Set the flag for the connected device to True
+            device_flags[index] = True
+            return True
+        except:
+            print(f"Connection to {device_mac} failed. Retrying...")
+            time.sleep(mr)
+            mr += 1
+    time.sleep(5)
+
+    print(f"Max connection retries reached for {device_mac}.")
+    return False
+
+
+def check_and_reconnect(device_mac, index):
+    while not rospy.is_shutdown():
+        status = check_bluetooth_device_status(device_mac)
+        print(f"Device {device_mac} status: {status}")
+
+        if status == "Disconnected":
+            ret_status = connect_with_retries(device_mac, adapters[index], index)
+            if ret_status:
+                print(f"Device {device_mac} reconnected successfully.")
+            else:
+                print(f"Device {device_mac} failed to reconnect.")
+
+        time.sleep(3)
+
+threads = []
+for i, device_mac in enumerate(device_macs):
+    thread = threading.Thread(target=check_and_reconnect, args=(device_mac, i))
+    thread.daemon = True
+    thread.start()
+    threads.append(thread)
+
+while not rospy.is_shutdown():
+    try:
+        time.sleep(1)
+    except rospy.ROSInterruptException:
+        for adapter in adapters:
+            adapter.stop()
+        break
+
+print('Calling the adapter close')
+for adapter in adapters:
+    adapter.stop()
